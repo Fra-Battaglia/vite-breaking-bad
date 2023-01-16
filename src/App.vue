03296eb72a086a1f3cc4d27db7522c3026292e29
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-01-16 10:36:19 +0100

    project setup done

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+	import { store } from './store.js';
+
+	export default {
+		components: {
+
+		}
+	}
+</script>
+
+<template>
+	
+</template>
+
+<style scoped lang="scss">
+
+	@use './components/styles/partials/variables' as *;
+	@use './components/styles/partials/mixins' as *;
+
+	@use './components/styles/generals.scss' as *;
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const store = reactive ({
+	cardsList: [],
+	url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?Archetype=ALien'
+});
